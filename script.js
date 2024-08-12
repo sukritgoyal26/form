bb979d4bae57af627f35504f54cbef1c4ebf8e18
Sukrit <sukritgoyal36@gmail.com>
2024-08-12 15:41:07 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+
+document.getElementById('Storeform').addEventListener('submit', function(e) {e.preventDefault();
+    let name = document.getElementById("ShopName").value;
+    let Location = document.getElementById("Location").value;
+    let GoogleMap = document.getElementById("GoogleMap").value;
+    let ContactNumber = document.getElementById("ContactNumber").value;
+    const formData = {
+        name :name,
+        Location : Location,
+        GoogleMap:GoogleMap,
+        ContactNumber:ContactNumber
+    }
+    if(name===""||location===""|| GoogleMap ==="" || ContactNumber==="" ){
+        alert("Missing values")
+    }else{
+    localStorage.setItem('formData'+ Date.now() ,JSON.stringify(formData));
+
+    alert("Data saved in storage")
+
+    document.getElementById("Storeform").reset();    
+    }
+    
+});
