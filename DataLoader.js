24a5ac65f784d36c99a63ff7cc6f6e3f542350c3
Sukrit <sukritgoyal36@gmail.com>
2024-08-13 05:23:24 +0530

    default data Added

---
+++ DataLoader.js
@@ -0,0 +1,8 @@
+const addDefaultData = () =>{
+    localStorage.setItem("formData1723504329059",`{"UserId":1723504329059,"name":"abc","Location":"Ludhiana","GoogleMap":"https://www.google.com/","ContactNumber":"6283581329"}`)
+localStorage.setItem("formData1723504343162",`{"UserId":1723504343162,"name":"cde","Location":"ads","GoogleMap":"https://www.google.com/","ContactNumber":"61315"}`)
+localStorage.setItem("formData1723504352706",`{"UserId":1723504352706,"name":"wafs","Location":"asf","GoogleMap":"https://www.google.com/","ContactNumber":"498465"}`)
+localStorage.setItem("formData1723504363786",`{"UserId":1723504363786,"name":"adacs","Location":"asdfsg","GoogleMap":"https://www.google.com/","ContactNumber":"9746512"}`)
+
+window.location.reload()
+}
